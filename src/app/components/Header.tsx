1246807d0acf9c730f0f666692fa0da96bb34f3b
almas-aftab-ahmed <almassiddiqui375@mail.com>
2024-11-09 01:26:03 -0800

    commit message

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Header() {
+  return (
+
+    <div className="h-[134px] bg-[#a29875] w-[1920px]   text-[75px]  font-[Rye] text-[#FFFFFF]">
+
+        Manzzari
+    
+    
+    </div>
+  )
+};
+
+export default Header;
+
